6d7f9d7fbc3b004fcc50ef06a474ff575c64599c
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2022-12-08 09:27:49 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@
 # time spent: 3.0 hrs
 
 
-from flask import Flask
+from flask import Flask, render_template, request, session
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
-    return "No hablo queso!"
+    return render_template('login.html')
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
