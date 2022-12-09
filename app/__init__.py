a4a50f9dab6e7f4936b6edbe092305be25c6b8b1
Craig Chen <cchen33@stuy.edu>
2022-12-09 09:19:25 -0500

    routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,40 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
-def hello_world():
-    print("the __name__ of this module is... ")
-    print(__name__)
+def login():
+
     return render_template('login.html')
+
+
+@app.route("/register")       #assign fxn to route
+def register():
+
+    return render_template('register.html')
+    
+    
+@app.route("/stocks")       #assign fxn to route
+def stocks():
+
+    return render_template('stocks.html')
+
+
+@app.route("/weather")       #assign fxn to route
+def weather():
+
+    return render_template('weather.html')
+
+
+@app.route("/news")       #assign fxn to route
+def news():
+
+    return render_template('news.html')
+
+
+@app.route("/todo")       #assign fxn to route
+def todo():
+
+    return render_template('todo.html')
+
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
