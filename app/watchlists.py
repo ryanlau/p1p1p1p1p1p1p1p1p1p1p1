21c14c50f361c78e53e41af67fc179e4eea4bb0e
Elizabeth Paperno <liza.paperno@gmail.com>
2022-12-12 09:26:44 -0500

    watchlists db work

--- app/watchlists.py
+++ app/watchlists.py
@@ -6,4 +6,30 @@ def createWatchlistTable():
     conn =  dbFuncs.establishConnection()
     c = conn[0]
     db = conn[1]
-    c.execute(f"DROP TABLE IF EXISTS watchlist")
+    c.execute("DROP TABLE IF EXISTS watchlist")
+    c.execute("CREATE TABLE watchlist (username TEXT, ticker TEXT)")
+    dbFuncs.disconnect(db)
+
+def addTicker(username, tick):
+    conn =  dbFuncs.establishConnection()
+    c = conn[0]
+    db = conn[1]
+    c.execute("INSERT INTO  watchlists VALUES (?,?)",(username,tick))
+    dbFuncs.disconnect(db)
+
+def getListTickers(username): 
+    conn =  dbFuncs.establishConnection()
+    c = conn[0]
+    db = conn[1]
+    vals = c.execute("SELECT ticker FROM watchlist").fetchall()
+    dbFuncs.disconnect(db)
+    formatted_tickers = []
+    for i in range(len(vals)): 
+        formatted_tickers.append(vals[i][0])
+    return formatted_tickers
+
+def removeTicker(username, tick):
+    return True
+
+def deleteWatchlistTable(): 
+    return True
