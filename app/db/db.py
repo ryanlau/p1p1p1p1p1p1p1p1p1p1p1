f92514bb08c07142f29ad4e8b113a5316315e742
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-12-12 21:51:14 -0500

    rm

--- app/db/db.py
+++ app/db/db.py
@@ -1,16 +1,5 @@
 import sqlite3
 
-def establishConnection():
-    DB_FILE="app/db/db"
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-    return (c, db)
-
-def disconnect(db):
-    db.commit() #save changes
-    db.close()  #close database
-
-
 def get_connection():
     conn = sqlite3.connect("app/db/db")
     return conn
