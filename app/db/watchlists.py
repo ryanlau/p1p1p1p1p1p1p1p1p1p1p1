395bbd099f58a07e6069f4f8a92b36f526daba63
Elizabeth Paperno <liza.paperno@gmail.com>
2022-12-14 08:48:23 -0500

    small changes

--- app/db/watchlists.py
+++ app/db/watchlists.py
@@ -5,31 +5,32 @@ except:
 
 
 def create_watchlist_table(): 
-    query_db("DROP TABLE IF EXISTS watchlist")
-    query_db("CREATE TABLE watchlist (username TEXT, ticker TEXT, company_name TEXT)")
+    query_db("DROP TABLE IF EXISTS watchlists")
+    query_db("CREATE TABLE watchlists (username TEXT, ticker TEXT, company_name TEXT)")
 
 
 def add_ticker(username, tick, comp_name):
-    query_db("INSERT INTO  watchlist VALUES (?,?,?)",(username,tick,comp_name))
+    query_db("INSERT INTO  watchlists VALUES (?,?,?)",(username,tick,comp_name))
 
 
 def get_watchlist(username): 
-    vals = query_db("SELECT ticker,company_name FROM watchlist WHERE username = ?",(username,), all=True)
+    vals = query_db("SELECT ticker,company_name FROM watchlists WHERE username = ?",(username,), all=True)
     return vals
 
 def remove_ticker(username, tick):
-    query_db("DELETE FROM watchlist WHERE username = ? AND ticker = ?",(username,tick))
+    query_db("DELETE FROM watchlists WHERE username = ? AND ticker = ?",(username,tick))
 
 
 def delete_watchlist_table(): 
-    query_db("DROP TABLE IF EXISTS watchlist")
+    query_db("DROP TABLE IF EXISTS watchlists")
     
 
 
+# LINES BELOW ONLY GET RUN IF "EXPLICITY RAN" with `python app/db/watchlists.py`
 if __name__ == "__main__":
     create_watchlist_table()
-    # add_ticker("epap", "AAPL", "Apple")
-    # add_ticker("epap", "GOOG", "google")
+    add_ticker("epap", "AAPL", "Apple")
+    add_ticker("epap", "GOOG", "google")
     print(get_watchlist("epap"))
     print(get_watchlist("r"))
     remove_ticker("epap", "AAPL")
