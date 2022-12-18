975bf45ff58a179b1e874ede66e7d6ab445b67c3
Hui Wang <xeraonomen@gmail.com>
2022-12-18 17:37:38 -0500

    bruh

--- app/api/news.py
+++ app/api/news.py
@@ -10,20 +10,5 @@ news_list = response["results"]
 def get_news_list():
     return news_list
 
-def get_news_title(i):
-    return news_list[i]["title"]
-
-def get_news_blurb(i):
-    return news_list[i]["abstract"]
-
-def get_news_url(i):
-    return news_list[i]["url"]
-
-def get_news_img(i):
-    return news_list[i]["multimedia"][0]["url"]
-
 if __name__ == "__main__":
-    print(get_news_title(0))
-    print(get_news_blurb(0))
-    print(get_news_url(0))
-    print(get_news_img(0))
+    print(news_list)
